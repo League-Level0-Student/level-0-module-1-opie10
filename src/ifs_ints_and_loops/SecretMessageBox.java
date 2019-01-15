9def902ec4e1867639bd153e55ecdc1eb7c6d746
opie10 <league@WTS21.attlocal.net>
2019-01-14 18:28:02 -0800

    day 1
    fun computers crazy

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -14,10 +14,15 @@ import javax.swing.JOptionPane;
  * 
  */
 
-public class SecretMessageBox {
+public class SecretMessageBox { 
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		String pass="opimil";
+		JOptionPane.showInputDialog(" What is your secret message?");
 	
+		
+	}
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
