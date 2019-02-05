2b8af42737d76260f3db4f61511ec11137998ca6
Tacoalarmclock <45108867+Tacoalarmclock@users.noreply.github.com>
2019-02-04 18:31:04 -0800

    week 4
    processing fun, done module 1

---
+++ src/myages.java
@@ -0,0 +1,26 @@
+
+public class myages {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		for (int i = 1; i < 100; i++) {
+
+			if ((2008 + i) == 2019) {
+				System.out.println("I am " + i + " this year");
+
+			} else if (i > 11) {
+				System.out.println("I will be " + i + " in " + (2008 + i) + ".");
+			} else {
+				System.out.println("I was " + i + " in " + (2008 + i) + ".");
+
+			}
+			
+
+			
+
+		}
+
+	}
+
+}
