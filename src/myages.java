96caf6fcd06fd3810ec6f8c92b6c1533cdfd6255
augie <augie@keatinge.com>
2024-01-24 14:42:43 -0800

    Update myages.java

--- src/myages.java
+++ src/myages.java
@@ -6,10 +6,10 @@ public class myages {
 		// TODO Auto-generated method stub
 		for (int i = 1; i < 100; i++) {
 
-			if ((2008 + i) == 2019) {
+			if ((2008 + i) == 2022) {
 				System.out.println("I am " + i + " this year");
 
-			} else if (i > 11) {
+			} else if (i > 13) {
 				System.out.println("I will be " + i + " in " + (2008 + i) + ".");
 			} else {
 				System.out.println("I was " + i + " in " + (2008 + i) + ".");
